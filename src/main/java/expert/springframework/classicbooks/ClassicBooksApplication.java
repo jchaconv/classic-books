e2c34a0666af4ec89e31617610238548c47ab90f
jchaconv <jcaaron.chacon@outlook.com>
2021-01-28 12:39:43 -0500

    Initial project add.

---
+++ src/main/java/expert/springframework/classicbooks/ClassicBooksApplication.java
@@ -0,0 +1,13 @@
+package expert.springframework.classicbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassicBooksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/expert/springframework/classicbooks/ClassicBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package expert.springframework.classicbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
