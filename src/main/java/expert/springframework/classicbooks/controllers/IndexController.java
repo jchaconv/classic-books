50cf7418cac2305c8c4e5d45ca78b440fd5f97e0
jchaconv <jcaaron.chacon@outlook.com>
2021-01-29 10:13:00 -0500

    Create index page and controller

---
+++ src/main/java/expert/springframework/classicbooks/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package expert.springframework.classicbooks.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"/", ""})
+    public String index() {
+        return "index";
+    }
+
+}
