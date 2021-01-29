b5b1fb7f532afdf09c077522b40e53efa008d984
jchaconv <jcaaron.chacon@outlook.com>
2021-01-29 12:30:20 -0500

    added owner list and updated controller.

--- src/main/java/expert/springframework/classicbooks/controllers/AuthorController.java
+++ src/main/java/expert/springframework/classicbooks/controllers/AuthorController.java
@@ -1,14 +1,26 @@
 package expert.springframework.classicbooks.controllers;
 
+import expert.springframework.classicbooks.services.AuthorService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/authors")
 @Controller
 public class AuthorController {
 
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listAuthors() {
+    public String listAuthors(Model model) {
+        /*El string "authors" es el campo que el model buscará
+        en el template. Es la lista que recorrerá. Debe agregarse.*/
+        model.addAttribute("authors", authorService.findAll());
+
         return "authors/index";
     }
 
