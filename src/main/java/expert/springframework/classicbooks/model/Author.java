12e91bc64bba39d522f4dca70330e4b4fa6f1693
jchaconv <jcaaron.chacon@outlook.com>
2021-01-31 22:27:20 -0500

    added contact properties to author

--- src/main/java/expert/springframework/classicbooks/model/Author.java
+++ src/main/java/expert/springframework/classicbooks/model/Author.java
@@ -4,7 +4,34 @@ import java.util.Set;
 
 public class Author extends Person {
 
+    private String address;
+    private String city;
+    private String telephone;
     Set<Book> books;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Book> getBooks() {
         return books;
@@ -13,4 +40,5 @@ public class Author extends Person {
     public void setBooks(Set<Book> books) {
         this.books = books;
     }
+
 }
