8665cdde0462e327634de1be0fc1b08e196dfb49
jchaconv <jcaaron.chacon@outlook.com>
2021-02-01 16:29:54 -0500

    updated base entity with jpa annotations

--- src/main/java/expert/springframework/classicbooks/model/BaseEntity.java
+++ src/main/java/expert/springframework/classicbooks/model/BaseEntity.java
@@ -1,10 +1,17 @@
 package expert.springframework.classicbooks.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
     //Clase creada para que otras puedan heredar el campo id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
