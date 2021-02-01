b4ba56da2e7e8eff8ae1c24001f84d32a8fa6492
jchaconv <jcaaron.chacon@outlook.com>
2021-01-31 22:25:12 -0500

    added editorial entity and associated with editor

--- src/main/java/expert/springframework/classicbooks/model/Editor.java
+++ src/main/java/expert/springframework/classicbooks/model/Editor.java
@@ -1,4 +1,16 @@
 package expert.springframework.classicbooks.model;
 
+import java.util.Set;
+
 public class Editor extends Person {
+
+    private Set<Editorial> editorials;
+
+    public Set<Editorial> getEditorials() {
+        return editorials;
+    }
+
+    public void setEditorials(Set<Editorial> editorials) {
+        this.editorials = editorials;
+    }
 }
---
+++ src/main/java/expert/springframework/classicbooks/model/Editorial.java
@@ -0,0 +1,14 @@
+package expert.springframework.classicbooks.model;
+
+public class Editorial extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
