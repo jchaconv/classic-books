fed15117709a181e8a5a4fc5b015997abff40592
jchaconv <jcaaron.chacon@outlook.com>
2021-01-31 22:22:54 -0500

    added publication, updated author with book relationship.

--- src/main/java/expert/springframework/classicbooks/model/Author.java
+++ src/main/java/expert/springframework/classicbooks/model/Author.java
@@ -1,4 +1,16 @@
 package expert.springframework.classicbooks.model;
 
+import java.util.Set;
+
 public class Author extends Person {
+
+    Set<Book> books;
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
 }
---
+++ src/main/java/expert/springframework/classicbooks/model/Publication.java
@@ -0,0 +1,34 @@
+package expert.springframework.classicbooks.model;
+
+import java.time.LocalDate;
+
+public class Publication extends BaseEntity {
+
+    private LocalDate date;
+    private String description;
+    private Book book;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+}
