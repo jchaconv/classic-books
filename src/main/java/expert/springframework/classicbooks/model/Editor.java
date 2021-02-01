c21c55817d99aae6b01b9704613f984f8f60a693
jchaconv <jcaaron.chacon@outlook.com>
2021-02-01 17:27:23 -0500

    updated Editor to use JPA

--- src/main/java/expert/springframework/classicbooks/model/Editor.java
+++ src/main/java/expert/springframework/classicbooks/model/Editor.java
@@ -1,10 +1,16 @@
 package expert.springframework.classicbooks.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "editors")
 public class Editor extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "editor_editorials", joinColumns = @JoinColumn(name = "editor_id"),
+            inverseJoinColumns = @JoinColumn(name = "editorial_id"))
     private Set<Editorial> editorials = new HashSet<>();
 
     public Set<Editorial> getEditorials() {
--- src/main/java/expert/springframework/classicbooks/model/Editorial.java
+++ src/main/java/expert/springframework/classicbooks/model/Editorial.java
@@ -1,7 +1,14 @@
 package expert.springframework.classicbooks.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "editorials")
 public class Editorial extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
